b0b1916f978d3e8cd6d53673a216a8660827d485
Your Name <you@example.com>
2020-08-26 15:57:35 -0700

    added eval script

---
+++ eval.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+set -x
+set -e
+export OUTUPT_DIR=/output
+export CGC_SOURCE=/home/haochen/work/cgc/cb-multios_gcov
+export CONFIG_DIR=/workdir/env/config
+
+function eval() {
+    NAME_PATH=$(echo $1 | cut -d '_' -f 1)_$(echo $1 | cut -d '_' -f 2)
+    VERSION=$2
+    NAME=$(echo $NAME_PATH | cut -d '/' -f 3)
+    CB_NAME=$(python $CONFIG_DIR/cb_dic.py -n $NAME)
+    cd $OUTUPT_DIR/${NAME}_${VERSION}
+    mv ./qsym/queue/* ./afl-master/queue
+    rm -rf ./qsym
+    /afl-cov/afl-cov -d $1 --coverage-cmd "${CGC_SOURCE}/${CB_NAME}_${VERSION}/build/challenges/${CB_NAME}/${CB_NAME} < AFL_FILE"  --code-dir ${CGC_SOURCE}/${CB_NAME}_${VERSION} --enable-branch-coverage --coverage-include-lines --clang --cov_all --overwrite
+}
+
+cd /workdir
+. job_pool.sh
+job_pool_init $1 0
+# compute lcov report
+for result_dir in $OUTUPT_DIR/*; do
+    if [ -d ${result_dir} ]; then
+        job_pool_run eval $result_dir $2
+    fi
+done
+job_pool_wait
+job_pool_shutdown
+exit 0
